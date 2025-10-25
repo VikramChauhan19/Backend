54f692f54c9f8c63000b39e6711b32385233c1bb
VikramChauhan19 <vikram9696chauhan@gmail.com>
2025-10-26 00:53:56 +0530

    4-a

--- 4-auth-app/routes/user.js
+++ 4-auth-app/routes/user.js
@@ -1,26 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-const {login, signup} = require("../controllers/Auth");
-const {auth, isStudent, isAdmin} = require("../middlewares/auth");
+const { login, signup } = require("../controllers/Auth");
+const { auth, isStudent, isAdmin } = require("../middlewares/auth");
 
-
-router.post("/signup",signup);
-router.post("/login",login);
-
+router.post("/signup", signup);
+router.post("/login", login);
 
 //protected route
-router.get("/student",auth, isStudent,(req,req)=>{
-    res.json({
-        success:true,
-        message:"You are welcome to the protected route for students"
-    });   
-})
-router.get("/admin",auth, isAdmin,(req,req)=>{
-    res.json({
-        success:true,
-        message:"You are welcome to the protected route for admins"
-    });   
-})
+router.get("/student", auth, isStudent, (req, res) => {
+  return res.json({
+    success: true,
+    message: "You are welcome to the protected route for students",
+  });
+});
+router.get("/admin", auth, isAdmin, (req, res) => {
+  return res.json({
+    success: true,
+    message: "You are welcome to the protected route for admins",
+  });
+});
 
 module.exports = router;
